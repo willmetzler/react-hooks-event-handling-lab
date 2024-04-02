d295c4f475def5ee63c73ee35e4c18e8c64367ef
Will Metzler <metzlerwill@gmail.com>
2024-04-02 13:01:07 -0400

    update components

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe () {
+    return (
+        <div>
+            <button onFocus={event => console.log('Good!')} onBlur={event => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
+import React from "react";
+
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={event => console.log('Entering password...')} />
+        </div>
     )
 }
 
